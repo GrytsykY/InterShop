ca67c5c9616b9fcf88c0230c3dde2ec0fd6a5e8f
Yurii Grytsyk <grytsykyura@gmail.com>
2020-03-02 11:10:43 +0200

    Initial commit

---
+++ config.php
@@ -0,0 +1,10 @@
+<?php
+defined('WG_ACCESS') or die('Access denied');
+
+const SITE_URL = 'http://InternetShop';
+const PATH = '/';
+
+const HOST = 'localhost';
+const USER = 'root';
+const PASS = '';
+const DB_NAME = 'intershop';
